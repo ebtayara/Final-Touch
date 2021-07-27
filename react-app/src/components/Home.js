6323db7a8e6c4ab6cc1ff5341b330eef71fa6c7d
Ebrahim Tayara <ebrahim@tayara.net>
2021-07-27 14:39:04 -0700

    creating home component

---
+++ react-app/src/components/Home.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import './styles/Home.css'
+
+const Home = () => {
+
+  
+}
