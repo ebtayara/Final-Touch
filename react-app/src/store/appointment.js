0a71ba6f83b28c787ad07486beabf57fe91efd1c
Ebrahim Tayara <ebrahim@tayara.net>
2021-07-28 16:49:44 -0700

    committing edits before EoD

--- react-app/src/store/appointment.js
+++ react-app/src/store/appointment.js
@@ -26,22 +26,23 @@ const removeAppointment = () => ({
 //thunks
 export const getData = (id) => async(dispatch) => {
     const response = await fetch(`/api/appointment/${id}`)
-
     if(response.ok) {
         const userAppointmentData = await response.json();
         dispatch(appointmentData(userAppointmentData));
     }
 }
 
-export const addAppointment = () => async(dispatch) => {
+export const addAppointment = (full_name, email, address, phone_number) => async(dispatch) => {
     const response = await fetch('/api/appointment/addAppointment', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(
-            
+            full_name,
+            email,
+            address,
+            phone_number
         )
     });
-
     if (response.ok) {
         const newAppointment = await response.json()
         dispatch(addAppointment(newAppointment));
