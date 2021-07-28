e8aa781c0fdf65f5eb28a97c01e0185e48921444
Ebrahim Tayara <ebrahim@tayara.net>
2021-07-28 12:37:36 -0700

    setting up store for appointments

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,7 +9,6 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import Home from './components/Home';
-
 import { authenticate } from './store/session';
 
 function App() {
