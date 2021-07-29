28856337736aef5df0079903718d7a62e4220806
Ebrahim Tayara <ebrahim@tayara.net>
2021-07-29 15:40:22 -0700

    edits to App component. created form

--- react-app/src/components/Appointment.js
+++ react-app/src/components/Appointment.js
@@ -1,11 +1,86 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, {useState} from 'react';
+import {NavLink} from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
-import { useDispatch, useSelector } from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux';
+import {addAppointment, updateAppointment, removeAppointment} from '../store/appointment';
 import './styling/Appointment.css';
 
 const Appointment = () => {
   const [] = useState()
   const user = useSelector(state => state.session.user)
-  
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [phoneNumber, setNumber] = useState('');
+  const dispatch = useDispatch();
+
+  const updateFullName = (e) => {
+    setFullName((e.target.value));
+  };
+
+  const updateEmail = (e) => {
+    setEmail((e.target.value));
+  };
+
+  const updateAddress = (e) => {
+    setAddress((e.target.value));
+  };
+
+  const updateNumber = (e) => {
+    setNumber((e.target.value));
+  };
+
+  return (
+    <div className='form_outer_container'>
+      <div className='form_inner_container'>
+        <form >
+          <div className='name_field'>
+            <label>Full Name</label>
+            <input
+              type='text'
+              name='full_name'
+              placeholder='Fist and Last Name'
+              onChange={updateFullName}
+              value={fullName}
+            ></input>
+          </div>
+          <div className='email_field'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='text'
+              name='email'
+              placeholder='email@ESP.domain'
+              onChange={updateEmail}
+              value={email}
+            ></input>
+          </div>
+          <div className='address_field'>
+            <label>Address</label>
+            <input
+              type='text'
+              name='address'
+              placeholder='Street, City and State'
+              onChange={updateAddress}
+              value={address}
+            ></input>
+          </div>
+          <div className='number_field'>
+            <label>Phone Number</label>
+            <input
+              type='integer'
+              name='phone_number'
+              placeholder='Enter 10 Digit Number'
+              onChange={updateNumber}
+              value={phoneNumber}
+            ></input>
+          </div>
+          <div className='submit_btn'>
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
+
+export default Appointment
