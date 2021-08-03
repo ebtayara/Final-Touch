40ece5faa40a6f42e50ffecb3dc6492c2c72f91e
Ebrahim Tayara <ebrahim@tayara.net>
2021-08-03 14:46:29 -0700

    setting up backend api

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session';
 import appointmentReducer from './appointment';
+import reviewsReducer from './reviews';
 
 const rootReducer = combineReducers({
   session,
   appointmentReducer,
+  reviewsReducer
 });
 
 
