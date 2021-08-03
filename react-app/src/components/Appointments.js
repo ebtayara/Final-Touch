9c62d340430ab64a0e00f39986661df9b5c47c93
Ebrahim Tayara <ebrahim@tayara.net>
2021-08-03 10:05:27 -0700

    edit form prefills previous data

--- react-app/src/components/Appointments.js
+++ react-app/src/components/Appointments.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {getAppointmentData} from '../store/appointment';
 import './styling/Appointments.css';
+
+useEffect(() => {
+  await dispatch(getAppointmentData(user_id))
+})
 
 const Appointments = () => {
   return (
