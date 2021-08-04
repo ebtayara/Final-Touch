a758ae2a97502ccb22c6b4f81c34ac2702fd4ab6
Ebrahim Tayara <ebrahim@tayara.net>
2021-08-04 08:27:00 -0700

    edits to backend api

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -4,4 +4,14 @@ from app.models import Review, db
 
 reviews_routes = Blueprint('reviews', __name__)
 
+#get comments
+@reviews_routes.route('/<int:id>')
+@login_required
+def grab_reviews(id):
+    reviews =
 
+#create comment
+@reviews_routes.route('/<int:id>')
+@login_required
+def create_review(id):
+    reviews =
