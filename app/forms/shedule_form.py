cb4c43b2b2c3aafb454e0d2dded67f1524077698
Ebrahim Tayara <ebrahim@tayara.net>
2021-08-09 01:32:22 -0700

    reverted db changes. attempting to fix broken routes

--- app/forms/shedule_form.py
+++ app/forms/shedule_form.py
@@ -10,4 +10,4 @@ class ScheduleForm(FlaskForm):
     address = StringField('address', validators=[DataRequired('Address is required')])
     phone_number = IntegerField('phone_number', validators=[DataRequired('Number is required'),
     # NumberRange(1000000000, None, 'Number needs to contain at least 10 digits')])
-    NumberRange(10009999, None, 'Number needs to contain at least 4 digits')])
+    NumberRange(10009999, None, 'Number needs to contain at least 8 digits')])
