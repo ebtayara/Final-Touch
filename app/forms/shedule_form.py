d16679fe15e47310bd9350330ce19684b860fb90
Ebrahim Tayara <ebrahim@tayara.net>
2021-08-09 00:17:25 -0700

    fixing heroku error

--- app/forms/shedule_form.py
+++ app/forms/shedule_form.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
 from wtforms import validators
-from wtforms.validators import DataRequired, ValidationError, Email, email_validator
+from wtforms.validators import DataRequired, ValidationError, Email
 # from app.models import Appointment
 
 class ScheduleForm(FlaskForm):
     full_name = StringField('full_name', validators=[DataRequired('Name is required')])
-    email = StringField('email', validators=[DataRequired('Email is required'), Email(email_validator)])
+    email = StringField('email', validators=[DataRequired('Email is required'), Email()])
     address = StringField('address', validators=[DataRequired('Address is required')])
     phone_number = IntegerField('phone_number', validators=[DataRequired('Number is required')])
