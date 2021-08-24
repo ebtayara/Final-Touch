ba79d02a95d6dc67e2939ef61ba75e694c16d80f
Ebrahim Tayara <ebrahim@tayara.net>
2021-08-24 14:23:11 -0700

    changed edit button to submit

--- react-app/src/components/Review.js
+++ react-app/src/components/Review.js
@@ -97,7 +97,7 @@ import './styling/Review.css';
                 {showForm && review.id === formId ?
                     <form onSubmit={(e) => updateReview(review.id, text_field, e)} key={review.id}>
                       <input type="text" value={text_field} onChange={(e) => setBody(e.target.value)} />
-                      <button className='edit_review' type='submit' onSubmit={(e) => updateReview(review.id, text_field, e)}>edit</button>
+                      <button className='edit_review' type='submit' onSubmit={(e) => updateReview(review.id, text_field, e)}>submit</button>
                       <button className='delete_review' onClick={() => deleteReview(review.id)}>delete</button>
                     </form>
                     : null}
