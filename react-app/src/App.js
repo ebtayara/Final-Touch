f3f2d7bc81d46ac19cec3918df1b1a4951ffc85b
Ebrahim Tayara <ebrahim@tayara.net>
2021-09-06 07:35:57 -0700

    made adjustments to how content is being displayed/layout

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,30 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
-import Splash from './components/Splash';
-import Home from './components/Home';
-import CarDetailing from './components/CarDetailing';
-import Appointment from './components/Appointment';
-import EditApp from './components/EditApp';
-import Appointments from './components/Appointments';
-import Review from './components/Review';
-import Reviews from './components/Reviews';
-import NotFound from './components/NotFound';
-import { authenticate } from './store/session';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import LoginForm from "./components/auth/LoginForm";
+import SignUpForm from "./components/auth/SignUpForm";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import UsersList from "./components/UsersList";
+import User from "./components/User";
+import Splash from "./components/Splash";
+import Home from "./components/Home";
+import CarDetailing from "./components/CarDetailing";
+import Appointment from "./components/Appointment";
+import EditApp from "./components/EditApp";
+import Appointments from "./components/Appointments";
+import Review from "./components/Review";
+import Reviews from "./components/Reviews";
+import NotFound from "./components/NotFound";
+import { authenticate } from "./store/session";
+import { Layout } from "./Layout";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       await dispatch(authenticate());
       setLoaded(true);
     })();
@@ -36,51 +35,53 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <Route path='/car-detailing' exact={true}>
-          <CarDetailing />
-        </Route>
-        <Route path='/appointments/:id' exact={true}>
-          <Appointment />
-        </Route>
-        <Route path='/reviews/appointments/:id' exact={true}>
-          <Review />
-        </Route>
-        <Route path='/edit/:id' exact={true}>
-          <EditApp />
-        </Route>
-        <ProtectedRoute path='/appointments' exact={true}>
-          <Appointments />
-        </ProtectedRoute>
-        <ProtectedRoute path='/reviews' exact={true}>
-          <Reviews />
-        </ProtectedRoute>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
-        <Route path='/' exact={true}>
-          <Splash />
-        </Route>
-        <Route path='/home' exact={true}>
-          <Home />
-        </Route>
-        <Route exact={true}>
-          <NotFound />
-        </Route>
-      </Switch>
-      <Footer />
+      <Layout>
+        <Switch>
+          <Route path="/login" exact={true}>
+            <LoginForm />
+          </Route>
+          <Route path="/sign-up" exact={true}>
+            <SignUpForm />
+          </Route>
+          <Route path="/car-detailing" exact={true}>
+            <CarDetailing />
+          </Route>
+          <Route path="/appointments/:id" exact={true}>
+            <Appointment />
+          </Route>
+          <Route path="/reviews/appointments/:id" exact={true}>
+            <Review />
+          </Route>
+          <Route path="/edit/:id" exact={true}>
+            <EditApp />
+          </Route>
+          <ProtectedRoute path="/appointments" exact={true}>
+            <Appointments />
+          </ProtectedRoute>
+          <ProtectedRoute path="/reviews" exact={true}>
+            <Reviews />
+          </ProtectedRoute>
+          <ProtectedRoute path="/users" exact={true}>
+            <UsersList />
+          </ProtectedRoute>
+          <ProtectedRoute path="/users/:userId" exact={true}>
+            <User />
+          </ProtectedRoute>
+          <Route path="/" exact={true}>
+            <Splash />
+          </Route>
+          <Route path="/home" exact={true}>
+            <Home />
+          </Route>
+          <Route exact={true}>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
---
+++ react-app/src/Layout.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import styled from "styled-components";
+
+export function Layout({ children }) {
+  return (
+    <LayoutStyles>
+      <NavBar />
+      <MainWithBackgroundImage>{children} </MainWithBackgroundImage>
+      <Footer />
+    </LayoutStyles>
+  );
+}
+const NAVBAR_HEIGHT = 64;
+// handles laying out 3 elements in a column, nav - main - div.footer
+const LayoutStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  nav {
+    height: ${NAVBAR_HEIGHT}px;
+  }
+  .main-content {
+    flex-grow: 1;
+    min-height: calc(100vh - ${NAVBAR_HEIGHT}px);
+    height: fit-content;
+    background-size: cover;
+    background-attachment: fixed;
+  }
+`;
+const BACKGROUND_IMAGES_PER_URL = {
+  "/car-detailing": "https://www.holtsauto.com/simoniz/wp-content/uploads/sites/4/2017/09/Fotolia_90003660_S.jpg",
+  "/appointments": "https://www.cooleydickinson.org/wp-content/uploads/2016/09/Appointment-Brand-Canvas-scaled.jpg",
+  "/appointments/:id": "https://www.motortrend.com/uploads/sites/5/2021/06/011-fast-and-furious-ford-escort-rs1600.jpg",
+  "/reviews/appointments/:id": "https://cdn.pocket-lint.com/r/s/970x/assets/images/148310-tv-feature-what-order-should-you-watch-the-fast-and-furious-films-in-image1-rzgajwfo2x.jpg",
+  "/reviews": "https://storage.googleapis.com/wordpress-www-vendasta/Top-14-Review-Sites-fb.jpg",
+  "/home": "https://images.squarespace-cdn.com/content/v1/5a7fb700d74cffd8f02428bf/1549039763632-Z29HWK0B1MPUE3YUKNXC/What-is-Mobile-Detailing-min.png?format=750w"
+};
+/** handles background image per url */
+function MainWithBackgroundImage({ children }) {
+  const { pathname } = useLocation();
+  const backgroundImageUrl = BACKGROUND_IMAGES_PER_URL[pathname];
+  return (
+    <main
+      className="main-content"
+      style={{ backgroundImage: `url(${backgroundImageUrl})` }}
+    >
+      {children}
+    </main>
+  );
+}
