4921aa6ee8815572b3475c03df821b53488f2afd
Ebrahim Tayara <ebrahim@tayara.net>
2021-09-06 22:37:55 -0700

    about me links now open in a new tab

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -8,14 +8,14 @@ const Footer = () => {
         <div className="developer_info">
           Developed by
         </div>
-        <a href="https://github.com/ebtayara">
+        <a href="https://github.com/ebtayara" target="_blank">
             <div className = "github_icon_container">
               <div className = "github_icon">
                 <i className="fab fa-github"></i>
               </div>
             </div>
         </a>
-        <a href="https://www.linkedin.com/in/ebrahim-tayara-b9b50871/">
+        <a href="https://www.linkedin.com/in/ebrahim-tayara-b9b50871/" target="_blank">
             <div className = "linkedin_icon_container">
               <div className = "linkedin_icon">
                 <i className="fab fa-linkedin"></i>
