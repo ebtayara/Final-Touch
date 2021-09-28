83216298754699a6c285a769d54e82ea3e163ce5
Ebrahim Tayara <ebrahim@tayara.net>
2021-09-28 13:34:30 -0700

    set up backend route

---
+++ app/api/search.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+from app.models import db, Service
+from flask_login import login_required
+
+#setting up blueprint
+search_route = Blueprint('search', __name__)
+
+@search_route.route('/all')
+@login_required
+def get_services():
+    all_services = Service.query.all()
+    data = all_services
+    result = [service_type.to_dict() for servie_type in data]
+    return {'services': result}
