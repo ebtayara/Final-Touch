e53bc5fc75699743d6c8d23a84f00453b5914b94
Ebrahim Tayara <ebrahim@tayara.net>
2021-09-29 16:55:33 -0700

    setting up backend api routes

---
+++ app/api/custom_service.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+from flask_login import login_required, current_user
+from app.models import Service, db
+
+service_routes = Blueprint('services', __name__)
+
+
+#get service
+
+#create service
+
+#edit service
+
+#delete service
