b25ce849b6909027cd1079d69fde9cfa48331757
Ebrahim Tayara <ebrahim@tayara.net>
2021-10-01 15:57:07 -0700

    made updates to schedule form

--- app/forms/shedule_form.py
+++ app/forms/shedule_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
 from wtforms import validators
+from wtforms.fields.core import SelectMultipleField
 from wtforms.validators import DataRequired, ValidationError, Email, NumberRange
 # from app.models import Appointment
 
@@ -15,6 +16,7 @@ class ScheduleForm(FlaskForm):
     email = StringField('email', validators=[DataRequired('Email is required.'), Email()])
     address = StringField('address', validators=[DataRequired('Address is required.')])
     phone_number = IntegerField('phone_number', validators=[DataRequired('Number is required.')])
+    service_type = SelectMultipleField('service_type')
     # NumberRange(min = 1, max = 15, message = 'Number cannot exceed 15 digits.')]
     # NumberRange(1000000000, None, 'Number needs to contain at least 10 digits')
     # NumberRange(10009999, None, 'Number needs to contain at least 8 digits')
